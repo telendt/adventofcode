6660256915c52ba8cbe5a7b38ce30d764c542eee
Tomasz Elendt <tomasz.elendt@gmail.com>
2015-12-12 13:27:23 +0100

    Add solution for day 12

---
+++ day12/solution.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import numbers
+import collections.abc
+import sys
+import json
+from typing import Any, Optional
+
+
+def sum_nums(obj: Any, *, skip_val: Optional[str]=None) -> int:
+    r"""
+
+    >>> sum_nums([1, 2, 3])
+    6
+    >>> sum_nums({"a":2,"b":4})
+    6
+    >>> sum_nums([[[3]]])
+    3
+    >>> sum_nums({"a":{"b":4},"c":-1})
+    3
+    >>> sum_nums([])
+    0
+    >>> sum_nums({})
+    0
+    >>> sum_nums([1,2,3], skip_val='red')
+    6
+    >>> sum_nums([1,{"c":"red","b":2},3], skip_val='red')
+    4
+    >>> sum_nums({"d":"red","e":[1,2,3,4],"f":5}, skip_val='red')
+    0
+    >>> sum_nums([1,"red",5], skip_val='red')
+    6
+    """
+    if isinstance(obj, numbers.Number):
+        return obj
+    else:
+        if isinstance(obj, collections.abc.Mapping):
+            obj = obj.values()
+            if skip_val is not None and skip_val in obj:
+                return 0
+        if isinstance(obj, collections.abc.Iterable) and \
+           not isinstance(obj, (str, bytes, bytearray)):
+            return sum(sum_nums(e, skip_val=skip_val)
+                       for e in obj)
+    return 0
+
+
+if __name__ == '__main__':
+    obj = json.load(sys.stdin)
+
+    print(sum_nums(obj))
+    print(sum_nums(obj, skip_val='red'))
